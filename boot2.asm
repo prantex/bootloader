6d217e2662c01ee7794b790699e4f477600cbf17
prantex <petterm0023@gmail.com>
2017-04-19 20:14:55 -0300

    Update boot2.asm

--- boot2.asm
+++ boot2.asm
@@ -6,45 +6,45 @@ strlen1: equ $-string1
  
 
 start:
-    XOR ax, ax
-    MOV ds, ax  ; reset 
+    xor AX, AX
+    mov DS, AX  ; reset 
 
-    MOV ah, 00h ; set video mode
-    MOV al, 03h ; text mode. 80x25. 16 colors. 8 pages
-    INT 10h     ; int video mode
+    mov AH, 00h ; set video mode
+    mov AL, 03h ; text mode. 80x25. 16 colors. 8 pages
+    int 10h     ; int video mode
 
-    MOV ah, 0xb ; set video color background
-    MOV bh, 0x0 ; ID da paleta de cores
-    MOV bl, 4   ; red
-    INT 10h
+    mov AH, 0xb ; set video color background
+    mov BH, 0x0 ; ID da paleta de cores
+    mov BL, 4   ; red
+    int 10h
       
-    MOV si, string1 ; moves string to SI
-    MOV ah, 0x0e    ; print char 
-    INT 0x10        ; video int
+    mov SI, string1 ; moves string to SI
+    mov AH, 0x0e    ; print char 
+    int 0x10        ; video int
       
     .loop lodsb
-          OR al,al  ; check if end of string
-          JZ halt   
-          INT 0x10  ; print char
-          JMP .loop ; go to next char
+          or AL,AL  ; check if end of string
+          jz halt   
+          int 0x10  ; print char
+          jmp .loop ; go to next char
     
     halt:           HLT
             
 Kernel:
 
     ;carrega o kernel no endereço 0x7e00
-    mov ax, 0x07e0  ;segmento do kernel
-    mov es, ax  
-    xor bx, bx      ;offset do kernel
+    mov AX, 0x07e0  ;segmento do kernel
+    mov ES, AX  
+    xor BX, BX      ;offset do kernel
     
 
-    ;Ler setor do disco
-    MOV ah, 0x02    ; já explicado no boot1
-    MOV al, 50  ; ler 50 setores
-    MOV ch, 0x00    ; trilha 0
-    MOV cl, 0x04    ; setor 4
-    MOV dh, 0x00    ; cabeça 0
-    MOV dl, 0x00    ; drive 
+    ;ler setor do disco
+    mov AH, 0x02    ; já explicado no boot1
+    mov AL, 50      ; ler 50 setores
+    mov CH, 0x00    ; trilha 0
+    mov CL, 0x04    ; setor 4
+    mov DH, 0x00    ; cabeça 0
+    mov DL, 0x00    ; drive 
     int 13h 
 
     jmp 0x07e0 : 0x0
